dbbc4c7f7b8a91ef90d017e0e4f2fd1211d92d6f
user0931 <joaquingonzalezlopez@yahoo.es>
2023-05-31 23:17:22 +0200

    Declaradas todas las funciones de los pulsadores

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+
+/* Libreria para la gestión de los pulsadores */
 #include <Pushbutton.h>
 
 /* Includes para la pantalla */
@@ -12,7 +14,7 @@
 /* Pantalla de 128*64 SPI */
 U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8); 
 
-/* Definimos los pulsadores y les asignamos el pin donde están conectados primer numero fila, segundo columna por ejemplo 1_5 fila 1 quinto pulsador*/
+/* Definimos los pulsadores y les asignamos el pin donde están conectados. Primer numero fila, segundo columna por ejemplo 1_5 fila 1 quinto pulsador*/
 #define DEF_BUTTON_1_1 22
 Pushbutton button_1_1(DEF_BUTTON_1_1);
 #define DEF_BUTTON_1_2 24
@@ -45,7 +47,7 @@ Pushbutton button_3_4(DEF_BUTTON_3_4);
 Pushbutton button_3_5(DEF_BUTTON_3_5);
 
 
-/* Definimos los leds y les asignamos el pin donde están conectados primer numero fila, segundo columna por ejemplo 1_5 fila 1 quinto led  */ 
+/* Definimos los leds y les asignamos el pin donde están conectados- Primer numero fila, segundo columna por ejemplo 1_5 fila 1 quinto led  */ 
 int led_1_1 = 23;
 int led_1_2 = 25;
 int led_1_3 = 27;
@@ -62,27 +64,27 @@ int led_3_3 = 47;
 int led_3_4 = 49;
 int led_3_5 = 51;
 
+const char * pos_1 = "2";
+const char * pos_2 = "A";
 
-String pos_1 = "11";
-String pos_2 = "22";
-
-//
-
-void draw1() {
+void draw1(char * pos_x,char * pos_y) {
   
   Serial.println("Estamos en función draw1");
   u8g2.clearBuffer();					// clear the internal memory
   //u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
   //u8g2.setFont(u8g2_font_ncenB24_tr);
-  u8g2.setFont(u8g2_font_inr42_mf);
+  // OK u8g2.setFont(u8g2_font_inr42_mf);
+  u8g2.setFont(u8g2_font_inr42_mr);
   u8g2.setCursor(15,10);
-  u8g2.print(pos_1);
-  //u8g2.drawStr(5,50,"A1");	// write something to the internal memory
+  //u8g2.print(pos_1);
+  // OK u8g2.drawStr(5,50,"A1");	// write something to the internal memory
+  u8g2.drawStr(5,50,pos_x);	// write something to the internal memory
+  // u8g2.drawStr(70,50,pos_2);	// write something to the internal memory
+  u8g2.drawStr(70,50,pos_y);	// write something to the internal memory
   u8g2.sendBuffer();					// transfer internal memory to the display
   delay(1000);  
   Serial.println("Fin de función draw1");
 }
-
 void draw2() {
   Serial.println("Estamos en función draw2");
   u8g2.clearBuffer();					// clear the internal memory
@@ -93,7 +95,6 @@ void draw2() {
   delay(1000);  
   Serial.println("Fin de función draw2");
 }
-
 void button_1_1_pressed() {
   Serial.print("Comienza función: ");
   Serial.println(__func__);
@@ -107,90 +108,106 @@ void button_1_1_pressed() {
   delay(200); // wait for a second  
   digitalWrite(led_1_2, LOW); // turn the LED off by making the voltage LOW  
   delay(10); // wait for a second
+  Serial_println("Pulsado boton 1 cambio Stomps")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
+  draw1("44", "z");
 }
 void button_1_2_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 2 cambio Stomps")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_1_3_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 3 cambio Stomps")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_1_4_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 4 cambio Stomps")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_1_5_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton incrementa performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_2_1_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 1 cambio Effects")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_2_2_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 2 cambio Effects")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_2_3_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 3 cambio Effects")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_2_4_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 4 cambio Effects")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_2_5_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton decrementa performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_3_1_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 1 cambio Performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_3_2_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 2 cambio Performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_3_3_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 3 cambio Performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_3_4_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 4 cambio Performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
 void button_3_5_pressed() { 
   Serial.print("Comienza función: ");
   Serial.println(__func__);
+  Serial_println("Pulsado boton 5 cambio Performance")
   Serial.print("Fin de la función: ");
   Serial.println(__func__);
 }
@@ -216,9 +233,9 @@ void parpadea() {
 
 }
 
-void setup()  
-{  
+void setup() {  
     Serial.begin(115200);
+    u8g2.begin();
     /* Inicializamos como output los pines a los que están conectados los leds*/
     pinMode(23, OUTPUT);
     pinMode(25, OUTPUT);
@@ -235,10 +252,8 @@ void setup()
     pinMode(47, OUTPUT);
     pinMode(49, OUTPUT);
     pinMode(51, OUTPUT);
-    u8g2.begin();
 }  
-void loop()   
-{  
+void loop() {  
    /* Si se pulsa algún pulsador llamamos a su función*/
    if (button_1_1.getSingleDebouncedPress())
    {
@@ -308,4 +323,3 @@ void loop()
   //button.waitForButton();
   //Serial.println("Estoy en 0040");
 }
-
