1e8f07ec149299197c3f6b61dd8d591daa7bbf93
user0931 <61436143+user0931@users.noreply.github.com>
2025-04-05 13:25:37 +0200

    Version OK V01

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <Wire.h>
 #endif
 /* Pantalla de 128*64 SPI */
-U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8); 
+U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R2, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8); 
 
 /* Definimos los pulsadores y les asignamos el pin donde están conectados. Primer numero fila, segundo columna por ejemplo 1_5 fila 1 quinto pulsador*/
 //#define DEF_BUTTON_1_1 26
@@ -69,22 +69,62 @@ bool est_3_3 = false;
 bool est_3_4 = false;
 bool est_3_5 = false;
 
-/* Variable con el texto del display */
 
-// char txt_disp;
+//   Estructura para los datos de cada pedal
 
+struct PEDAL
+   {   byte  CCMidi     ;      // CC midi
+       byte  CCValor    ;      // Valor del CC midi
+       byte  CCCanal    ;      // Canal midi
+       char* Texto1     ;      // Texto en linea1 de la pantalla
+       char* Texto2     ;      // Texto en linea2 de la pantalla
+       bool  Persis_Led ;      // Indica si el led debe quedarse encendido o no
+   } ;
+
+// Cuando hay que enviar un valor para activarlo y otro para desactivarlo.
+
+PEDAL P1_1_ON  = {60,127,1,"Tuner","On",true};           // Activación
+PEDAL P1_1_OFF = {60,0,1,"Tuner","Off",false};       // Desactivación
+PEDAL P1_2_ON  = {79,0,1,"FS1","",true};             // Activación
+PEDAL P1_2_OFF = P1_2_ON;                              // Desactivación
+PEDAL P1_3_ON  = {80,0,1,"FS2","",true};             // Activación
+PEDAL P1_3_OFF = P1_3_ON;                              // Desactivación
+PEDAL P1_4_ON  = {81,0,1,"FS3","",true};             // Activación
+PEDAL P1_4_OFF = P1_4_ON;                              // Desactivación
+PEDAL P1_5_ON  = {29,0,1,"Mode 1","Footsw",true};      // Activación
+PEDAL P1_5_OFF = {29,127,1,"Mode 2","FX Chain",true};  // Desactivación
+PEDAL P2_1_ON  = {28,0,1,"  Patch ","  Mode ",true};   // Activación
+PEDAL P2_1_OFF = {28,127,1,"  Stomp","  Mode ",false};   // Desactivación
+PEDAL P2_2_ON  = {13,0,1,"Pedal 1","On",true};         // Activación
+PEDAL P2_2_OFF = {13,127,1,"Pedal 2","On",false};      // Desactivación
+PEDAL P2_3_ON  = {22,0,1,"Bank -","",true};          // Activación
+PEDAL P2_3_OFF = P1_3_ON;                              // Desactivación
+PEDAL P2_4_ON  = {23,0,1,"Bank +","",true};          // Activación
+PEDAL P2_4_OFF = P1_4_ON;                              // Desactivación
+PEDAL P2_5_ON  = {13,0,1,"EXP 1","",true};  // Activación
+PEDAL P2_5_OFF = {13,127,1,"EXP 2","",true};                              // Desactivación
+PEDAL P3_1_ON  = {26,0,1,"Patch -","",true};           // Activación
+PEDAL P3_1_OFF = P3_1_ON;                              // Desactivación
+PEDAL P3_2_ON  = {27,0,1,"Patch +","",true};           // Activación
+PEDAL P3_2_OFF = P1_2_ON;                              // Desactivación
+PEDAL P3_3_ON  = {25,1,1,"Scene 1","   On",true};      // Activación
+PEDAL P3_3_OFF = P1_3_ON;                              // Desactivación
+PEDAL P3_4_ON  = {25,2,1,"Scene 2","   On",true};      // Activación
+PEDAL P3_4_OFF = P1_4_ON;                              // Desactivación
+PEDAL P3_5_ON  = {25,3,1,"Scene 3","   On",true};      // Activación
+PEDAL P3_5_OFF = P1_5_ON;                              // Desactivación
+
+/* Variables con el texto del display */
 const char * pos_1 = "2";
 const char * pos_2 = "A";
 
+/* Variables para almacenar estado de FS y Escena*/
+int estado_fs;
+int estado_escena;
+
 /* Librería para MIDI */
 #include <MIDI.h>
-//MIDI_CREATE_DEFAULT_INSTANCE();
 MIDI_CREATE_INSTANCE(HardwareSerial,Serial, midiOut); // create a MIDI object called midiOut
-
-/*  Variables para MIDI */
-//  int CCMidi = 60;
-//  int CCMidiValue;
-int CHMidi = 1;
 
 void draw1(char * pos_x,char * pos_y) {
   
@@ -182,170 +222,159 @@ void test_led(){
   //Serial.println(__func__);
 }
 void button_1_1_pressed() {
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 1 cambio Stomps");
-  if (est_1_1 == false) {
-     envia_cc_midi(60,0,CHMidi);
-     digitalWrite(led_1_1, HIGH);
-     draw2("Tuner","1");
-     est_1_1 = true;
-  } else {
-    envia_cc_midi(60,127,CHMidi);
-    digitalWrite(led_1_1, LOW);
-    draw2("      ","2");
-    est_1_1 = false;
-  }
-
-  // parpadea(led_1_1);
-
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
-  
-  //draw1("44", "z");
-  // Envia comando Midi
-  envia_cc_midi(60,0,1);
+  envia_cc_midi(P1_1_ON.CCMidi,P1_1_ON.CCValor,P1_1_ON.CCCanal);
+  digitalWrite(led_1_1, HIGH);
+  draw2(P1_1_ON.Texto1,P1_1_ON.Texto2);
+  est_1_1 = true;
+  button_1_1.waitForPress();
+  envia_cc_midi(P1_1_OFF.CCMidi,P1_1_OFF.CCValor,P1_1_OFF.CCCanal);
+  digitalWrite(led_1_1, LOW);
+  draw2(P1_1_OFF.Texto1,P1_1_OFF.Texto2);
+  est_1_1 = false;
 }
-void button_1_2_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 2 cambio Stomps");
-  envia_cc_midi(79,0,CHMidi);
-  // parpadea(led_1_2);
-  // draw2("Stomp","2");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
-  //draw1("44", "z");
+void button_1_2_pressed() {
+    envia_cc_midi(P1_2_ON.CCMidi,P1_2_ON.CCValor,P1_2_ON.CCCanal);
+    estado_fs = 1;
+    digitalWrite(led_1_2, HIGH);
+    digitalWrite(led_1_3, LOW);
+    digitalWrite(led_1_4, LOW);
+    draw2(P1_2_ON.Texto1,P1_2_ON.Texto2);
 }
 void button_1_3_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 3 cambio Stomps");
-  // parpadea(led_1_3);
-  envia_cc_midi(80,0,CHMidi);
-  // draw2("Stomp","3");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P1_3_ON.CCMidi,P1_3_ON.CCValor,P1_3_ON.CCCanal);
+  estado_fs = 2;
+  digitalWrite(led_1_2, LOW);
+  digitalWrite(led_1_3, HIGH);
+  digitalWrite(led_1_4, LOW);
+  draw2(P1_3_ON.Texto1,P1_3_ON.Texto2);
 }
 void button_1_4_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 4 cambio Stomps");
-  // parpadea(led_1_4);
-  envia_cc_midi(81,0,CHMidi);
-  //draw2("Stomp","4");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P1_4_ON.CCMidi,P1_4_ON.CCValor,P1_4_ON.CCCanal);
+  estado_fs = 3;
+  digitalWrite(led_1_2, LOW);
+  digitalWrite(led_1_3, LOW);
+  digitalWrite(led_1_4, HIGH);
+  draw2(P1_4_ON.Texto1,P1_4_ON.Texto2);
 }
 void button_1_5_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton incrementa performance");
-  //parpadea(led_1_5);
-  envia_cc_midi(29,0,CHMidi);
-  //draw2("Perform","+");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  if (est_1_5 == false) {
+    envia_cc_midi(P1_5_ON.CCMidi,P1_5_ON.CCValor,P1_5_ON.CCCanal);
+    digitalWrite(led_1_5, HIGH);
+    draw2(P1_5_ON.Texto1,P1_5_ON.Texto2);
+    delay(1000);
+    digitalWrite(led_1_5, LOW);
+    est_1_5 = true;
+  } else {
+    envia_cc_midi(P1_5_OFF.CCMidi,P1_5_OFF.CCValor,P1_5_OFF.CCCanal);
+    digitalWrite(led_1_5, HIGH);
+    draw2(P1_5_OFF.Texto1,P1_5_OFF.Texto2);
+    delay(1000);
+    digitalWrite(led_1_5, LOW);
+    est_1_5 = false;
+  }
 }
-void button_2_1_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 1 cambio Effects");
-  //parpadea(led_2_1);
-  envia_cc_midi(28,0,CHMidi);
-  //draw2("Efects","1");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+void button_2_1_pressed() {
+  if (est_2_1 == false) {
+    envia_cc_midi(P2_1_ON.CCMidi,P2_1_ON.CCValor,P2_1_ON.CCCanal);
+    digitalWrite(led_2_1, HIGH);
+    draw2(P2_1_ON.Texto1,P2_1_ON.Texto2);
+    delay(1000);
+    digitalWrite(led_2_1, LOW);
+    est_2_1 = true;
+  } else {
+    envia_cc_midi(P2_1_OFF.CCMidi,P2_1_OFF.CCValor,P2_1_OFF.CCCanal);
+    digitalWrite(led_2_1, HIGH);
+    draw2(P2_1_OFF.Texto1,P2_1_OFF.Texto2);
+    delay(1000);
+    digitalWrite(led_2_1, LOW);
+    est_2_1 = false;
+  }
 }
 void button_2_2_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 2 cambio Effects");
-  //parpadea(led_2_2);
-  envia_cc_midi(28,127,CHMidi);
-  //draw2("Efects","2");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  if (est_2_2 == false) {
+    envia_cc_midi(P2_2_ON.CCMidi,P2_2_ON.CCValor,P2_2_ON.CCCanal);
+    digitalWrite(led_2_2, HIGH);
+    draw2(P2_2_ON.Texto1,P2_2_ON.Texto2);
+    delay(1000);
+    digitalWrite(led_2_2, LOW);
+    est_2_2 = true;
+  } else {
+    envia_cc_midi(P2_2_OFF.CCMidi,P2_2_OFF.CCValor,P2_2_OFF.CCCanal);
+    digitalWrite(led_2_2, HIGH);
+    draw2(P2_2_OFF.Texto1,P2_2_OFF.Texto2);
+    delay(1000);
+    digitalWrite(led_2_2, LOW);
+    est_2_2 = false;
+  }
 }
 void button_2_3_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 3 cambio Effects");
-  //parpadea(led_2_3);
-  envia_cc_midi(22,0,CHMidi);
-  //draw2("Efects","3");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P2_3_ON.CCMidi,P2_3_ON.CCValor,P2_3_ON.CCCanal);
+  digitalWrite(led_2_3, HIGH);
+  draw2(P2_3_ON.Texto1,P2_3_ON.Texto2);
+  delay(500);
+  digitalWrite(led_2_3, LOW);
 }
 void button_2_4_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 4 cambio Effects");
-  //parpadea(led_2_4);
-  envia_cc_midi(23,0,CHMidi);
-  //draw2("Efects","4");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P2_4_ON.CCMidi,P2_4_ON.CCValor,P2_4_ON.CCCanal);
+  digitalWrite(led_2_4, HIGH);
+  draw2(P2_4_ON.Texto1,P2_4_ON.Texto2);
+  delay(500);
+  digitalWrite(led_2_4, LOW);
 }
 void button_2_5_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton decrementa performance");
-  // parpadea(led_2_5);
-  envia_cc_midi(29,127,CHMidi);
-  //draw2("Perform","-");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  if (est_2_5 == false) {
+    envia_cc_midi(P2_5_ON.CCMidi,P2_5_ON.CCValor,P2_5_ON.CCCanal);
+    digitalWrite(led_2_5, HIGH);
+    draw2(P2_5_ON.Texto1,P2_5_ON.Texto2);
+    delay(1000);
+    digitalWrite(led_2_5, LOW);
+    est_2_5 = true;
+  } else {
+    envia_cc_midi(P1_5_OFF.CCMidi,P1_5_OFF.CCValor,P1_5_OFF.CCCanal);
+    digitalWrite(led_1_5, HIGH);
+    draw2(P1_5_OFF.Texto1,P1_5_OFF.Texto2);
+    delay(1000);
+    digitalWrite(led_1_5, LOW);
+    est_1_5 = false;
+  }
 }
 void button_3_1_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 1 cambio Performance");
-  // parpadea(led_3_1);
-  envia_cc_midi(26,0,CHMidi);
-  //draw2("Perform","1");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P3_1_ON.CCMidi,P3_1_ON.CCValor,P3_1_ON.CCCanal);
+  digitalWrite(led_3_1, HIGH);
+  draw2(P3_1_ON.Texto1,P3_1_ON.Texto2);
+  delay(500);
+  digitalWrite(led_3_1, LOW);
 }
 void button_3_2_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 2 cambio Performance");
-  // parpadea(led_3_2);
-  envia_cc_midi(27,0,CHMidi);
-  //draw2("Perform","2");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P3_2_ON.CCMidi,P3_2_ON.CCValor,P3_2_ON.CCCanal);
+  digitalWrite(led_3_2, HIGH);
+  draw2(P3_2_ON.Texto1,P3_2_ON.Texto2);
+  delay(500);
+  digitalWrite(led_3_2, LOW);
 }
 void button_3_3_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 3 cambio Performance");
-  //parpadea(led_3_3);
-  envia_cc_midi(25,1,CHMidi);
-  //draw2("Perform","3");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P3_3_ON.CCMidi,P3_3_ON.CCValor,P3_3_ON.CCCanal);
+  estado_escena = 1;
+  digitalWrite(led_3_3, HIGH);
+  digitalWrite(led_3_4, LOW);
+  digitalWrite(led_3_5, LOW);
+  draw2(P3_3_ON.Texto1,P3_3_ON.Texto2);
 }
 void button_3_4_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 4 cambio Performance");
-  //parpadea(led_3_4);
-  envia_cc_midi(25,2,CHMidi);
-  //draw2("Perform","4");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P3_4_ON.CCMidi,P3_4_ON.CCValor,P3_4_ON.CCCanal);
+  estado_escena = 2;
+  digitalWrite(led_3_3, LOW);
+  digitalWrite(led_3_4, HIGH);
+  digitalWrite(led_3_5, LOW);
+  draw2(P3_4_ON.Texto1,P3_4_ON.Texto2);
 }
 void button_3_5_pressed() { 
-  //Serial.print("Comienza función: ");
-  //Serial.println(__func__);
-  //Serial.println("Pulsado boton 5 cambio Performance");
-  //parpadea(led_3_5);
-  envia_cc_midi(25,3,CHMidi);
-  //draw2("Perform","5");
-  //Serial.print("Fin de la función: ");
-  //Serial.println(__func__);
+  envia_cc_midi(P3_5_ON.CCMidi,P3_5_ON.CCValor,P3_5_ON.CCCanal);
+  estado_escena = 3;
+  digitalWrite(led_3_3, LOW);
+  digitalWrite(led_3_4, LOW);
+  digitalWrite(led_3_5, HIGH);
+  draw2(P3_5_ON.Texto1,P3_5_ON.Texto2);
 }
 void setup() {  
     // Serial.begin(115200);
@@ -370,12 +399,6 @@ void setup() {
     pinMode(led_3_5, OUTPUT);
     //                  Lanzamos un test de leds donde se iluminarań todos en secuencia
     test_led();
-    draw2("666666","222222");
-    delay(1000);
-    draw2("7777777","2222222");
-    delay(1000);
-    draw2("12345678","12345678");
-    delay(1000);
 }  
 
 
